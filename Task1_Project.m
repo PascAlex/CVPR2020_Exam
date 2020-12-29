df49a89499380b2199c409d25429100c7fafdfc7
lorenzobonin <63099025+lorenzobonin@users.noreply.github.com>
2020-12-29 19:12:41 +0100

    Add files via upload

---
+++ Task1_Project.m
@@ -0,0 +1,80 @@
+
+close all force
+
+project = fullfile('dataset','train');
+
+imds = imageDatastore(project, ...
+    'IncludeSubfolders',true,'LabelSource','foldernames');
+
+% automatic resizing
+imds.ReadFcn = @(x)imresize(imread(x),[64 64]);
+
+% split in training and validation sets: 85% - 15%
+quotaForEachLabel=0.85;
+[imdsTrain,imdsValidation] = splitEachLabel(imds,quotaForEachLabel,'randomize');
+
+%% Network design and training
+% create the structure of the network
+layers = [
+    imageInputLayer([64 64 1],'Name','input','Normalization','zscore') 
+    
+    convolution2dLayer(3,8,'Name','conv_1') 
+    
+    reluLayer('Name','relu_1')
+
+    maxPooling2dLayer(2,'Stride',2,'Name','maxpool_1')
+    
+    convolution2dLayer(3,16,'Name','conv_2')
+    
+    reluLayer('Name','relu_2')
+    
+    maxPooling2dLayer(2,'Stride',2,'Name','maxpool_2')
+    
+    convolution2dLayer(3,32,'Name','conv_3') 
+   
+    reluLayer('Name','relu_3')
+    
+    fullyConnectedLayer(15,'WeightsInitializer','narrow-normal','Name','fc_1')
+    
+    softmaxLayer('Name','softmax')
+    
+    classificationLayer('Name','output')];
+
+    lgraph = layerGraph(layers); % to run the layers need a name
+    analyzeNetwork(lgraph)
+    
+% training options
+
+options = trainingOptions('sgdm', ...
+    'ValidationData',imdsValidation, ...
+    'ValidationFrequency', 10, ...
+    'ValidationPatience', 5,...
+    'Verbose',false, ...
+    'MiniBatchSize',32, ...
+    'ExecutionEnvironment','parallel',...
+    'Plots','training-progress')
+
+% training
+
+net = trainNetwork(imdsTrain,layers,options);
+
+%% evaluate performance on test set
+
+project_test  = fullfile('dataset','test');
+
+imdsTest = imageDatastore(project_test, ...
+    'IncludeSubfolders',true,'LabelSource','foldernames');
+imdsTest.ReadFcn = @(x)imresize(imread(x),[64 64]);
+
+% apply the network to the test set
+YPredicted = classify(net,imdsTest);
+YTest = imdsTest.Labels;
+
+% overall accuracy
+accuracy = sum(YPredicted == YTest)/numel(YTest)
+
+% confusion matrix
+figure
+plotconfusion(YTest,YPredicted);
+
+% accuracy = 0.35
