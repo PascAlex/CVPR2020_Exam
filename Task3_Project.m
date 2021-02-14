351fcc6813adcf2d25b4d6bc4f2a200767dec76d
nickplas <plasencia.milton@gmail.com>
2021-02-14 12:28:51 +0100

    Task 3

--- Task3_Project.m
+++ Task3_Project.m
@@ -5,12 +5,11 @@ clc
 
 %% Transfer learning
 %Download it form adds-on link
-% By defaukt should download imagenet weights
+% By default should download imagenet weights
 net = alexnet;
 
 %% Data Preprocessing
 %The input layer take [227 227 3]-sized images
-
 project = fullfile('dataset','train');
 
 imds = imageDatastore(project, ...
@@ -18,15 +17,32 @@ imds = imageDatastore(project, ...
 
 imds.ReadFcn = @(x)repmat(imresize(imread(x),[227 227]), 1,1,3);
 
+%% Only cropping
+
+cropped_project = fullfile('dataset','train_cropped');
+
+imdsCropped2 = imageDatastore(cropped_project, ...
+    'IncludeSubfolders',true,'LabelSource','foldernames');
+
+imdsCropped2.ReadFcn = @(x)repmat(imresize(imread(x),[227 227]), 1,1,3);
+%% Only Reflection 
+
+reflected_project = fullfile('dataset','train_reflected');
+
+imdsReflected2 = imageDatastore(reflected_project, ...
+    'IncludeSubfolders',true,'LabelSource','foldernames');
+
+imdsReflected2.ReadFcn = @(x)repmat(imresize(imread(x),[227 227]), 1,1,3);
 %% Splitting
 quotaForEachLabel=0.85;
 [imdsTrain,imdsValidation] = splitEachLabel(imds,quotaForEachLabel,'randomize');
 
-%% Data Augmentation
-augmenter = imageDataAugmenter(...
-            'RandXReflection', true);
-augmented_train = augmentedImageDatastore([227 227 3], imdsTrain, 'DataAugmentation', augmenter);
-             
+%% Data augmentation
+
+imdsAugmented = imageDatastore(cat(1,imdsTrain.Files,imdsReflected2.Files));
+imdsAugmented.Labels = cat(1,imdsTrain.Labels, imdsReflected2.Labels);
+
+imdsAugmented.ReadFcn = @(x)repmat(imresize(imread(x),[227 227]), 1,1,3);
 %% Resize test set
 project_test  = fullfile('dataset','test');
 
@@ -51,7 +67,7 @@ end
 %train the network and fine tune the last weights
 
 layers = [layers_to_transfer
-           %parametri messi a caso , DA CAMBIARE
+           
            fullyConnectedLayer(15, 'WeightLearnRateFactor', 2, ...
                                 'BiasLearnRateFactor', 5)
            
@@ -62,18 +78,18 @@ layers = [layers_to_transfer
 %% Options
 options = trainingOptions('adam', ...    
     'ValidationData',imdsValidation, ...
-    'MaxEpochs', 50, ...
-    'InitialLearnRate', 0.0001, ...
-    'ValidationPatience',5,...
+    'MaxEpochs', 30, ...
+    'InitialLearnRate', 0.001, ...
+    'ValidationPatience',5 ,...
     'Verbose',false, ...
     'Shuffle', 'every-epoch', ...
-    'MiniBatchSize',64, ... %provare con 128
+    'MiniBatchSize',64, ... 
     'ExecutionEnvironment','parallel',...
     'Plots','training-progress');
 
 %% Train the network
 
-net_trained = trainNetwork(augmented_train, layers, options);
+net_trained = trainNetwork(imdsAugmented, layers, options);
 
 %%
 %Look at the output + accuracy
@@ -88,14 +104,14 @@ accuracy = sum(YPredicted == YTest)/numel(YTest)
 figure
 plotconfusion(YTest,YPredicted)
 
-% accuracy around 85%
+%Accuracy finale: 86% (84/85 random cropping) 
 
 %% SVM
 
 %Feature Extraction
 
-feature_train = activations(net, augmented_train, 'fc7', 'OutputAs', 'rows');
-feature_test = activations(net, imdsTest, 'fc7', 'OutputAs', 'rows');
+feature_train = activations(net, imdsTrain, 'fc6', 'OutputAs', 'rows'); 
+feature_test = activations(net, imdsTrain, 'fc6', 'OutputAs', 'rows');
 
 %Extract labels
 YTrain1 = imdsTrain.Labels;
@@ -108,4 +124,11 @@ YPred1 = predict(classifier, feature_test);
 %Accuracy
 accuracy1 = mean(YPred1 == YTest1)
 
-%accuracy around 83/84%
+% acc = 0.73 conv5
+% acc = 0.87 fc6 0.84 w data augmenter
+% acc = 0.86 fc7 0.86 w data augmenter
+% acc = 0.84 fc8 0.84 w data augmenter
+
+%% plot confusion
+figure
+plotconfusion(YTest1,YPred1)
